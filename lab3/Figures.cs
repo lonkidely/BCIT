903abca5fca2e7196fe6501b3d14d912b4d4b524
lonkidely <lonkidely@gmail.com>
2020-10-29 19:26:47 +0300

    Added 3-rd lab

---
+++ lab3/Figures.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Figures
+{
+    abstract class GeometricFigure : IComparable
+    {
+        public string Type { get; protected set; }
+
+        public abstract double Area();
+
+        public override string ToString()
+        {
+            return this.Type + " площадью " + this.Area().ToString();
+        }
+
+        public int CompareTo(object obj)
+        {
+            GeometricFigure right = (GeometricFigure)obj;
+
+            if (this.Area() < right.Area()) 
+                return -1;
+            else if (this.Area() == right.Area()) 
+                return 0;
+            else 
+                return 1;
+        }
+    }
+
+    interface IPrint
+    {
+        void Print();
+    }
+
+    class Rectangle : GeometricFigure, IPrint
+    {
+
+        public double Height { get; protected set; }
+        public double Width { get; protected set; }
+
+        public Rectangle(double ph, double pw)
+        {
+            this.Height = ph;
+            this.Width = pw;
+            this.Type = "Прямоугольник";
+        }
+
+        public override double Area()
+        {
+            return (this.Height * this.Width);
+        }
+
+        public override string ToString()
+        {
+            return this.Type + " шириной " + this.Width.ToString() + ", высотой " + this.Height.ToString() + ", площадью " + this.Area().ToString();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+
+    class Square : Rectangle, IPrint
+    {
+        public Square(double size) : base(size, size)
+        {
+            this.Type = "Квадрат";
+        }
+
+        public override string ToString()
+        {
+            return this.Type + " со стороной " + this.Width.ToString() + ", площадью " + this.Area().ToString();
+        }
+    }
+
+    class Circle : GeometricFigure, IPrint
+    {
+        public double Radius { get; protected set; }
+
+        public Circle(double pr)
+        {
+            this.Radius = pr;
+            this.Type = "Круг";
+        }
+
+        public override double Area()
+        {
+            return this.Radius * this.Radius * Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return this.Type + " радиусом " + this.Radius.ToString() + ", площадью " + this.Area().ToString();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Figures;
+using Matrix;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание объектов классов фигур
+            Rectangle rectangle = new Rectangle(3, 4);
+            Square square = new Square(5);
+            Circle circle = new Circle(2.7282);
+
+
+            ArrayList array = new ArrayList
+            {
+                rectangle,
+                square,
+                circle
+            };
+
+            Console.WriteLine("Array list перед сортировкой");
+            foreach (var elem in array)
+                Console.WriteLine(elem);
+            
+
+            array.Sort();
+
+            Console.WriteLine("\nArray list после сортировки");
+            foreach (var elem in array)
+                Console.WriteLine(elem);
+
+
+            List<GeometricFigure> list = new List<GeometricFigure>
+            {
+                rectangle,
+                square,
+                circle
+            };
+
+            Console.WriteLine("\n\nList перед сортировкой");
+            foreach (var elem in array)
+                Console.WriteLine(elem);
+
+            list.Sort();
+
+            Console.WriteLine("\nList после сортировки");
+            foreach (var elem in array)
+                Console.WriteLine(elem);
+
+            Console.WriteLine("\n\nМатрица");
+            Matrix<GeometricFigure> matrix = new Matrix<GeometricFigure>(3, 3, 3, new FigureMatrixCheckEmpty());
+            matrix[0, 0, 0] = rectangle;
+            matrix[1, 1, 1] = square;
+            matrix[2, 2, 2] = circle;
+            Console.WriteLine(matrix.ToString());
+
+            try
+            {
+                GeometricFigure tmp = matrix[66, 1777, 11];
+            }
+            catch (ArgumentOutOfRangeException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+
+            Console.WriteLine("\n\nСтек");
+
+            SimpleStack<GeometricFigure> stack = new SimpleStack<GeometricFigure>();
+            stack.Push(rectangle);
+            stack.Push(square);
+            stack.Push(circle);
+
+            while (stack.Count > 0)
+            {
+                GeometricFigure f = stack.Pop();
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
