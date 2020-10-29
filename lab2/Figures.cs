048c6cdc887c1846e842c4fb7539b73d80a73186
lonkidely <lonkidely@gmail.com>
2020-10-29 16:29:41 +0300

    Added 2-nd lab

---
+++ lab2/Figures.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Figures
+{
+    abstract class GeometricFigure
+    {
+        public string Type { get; protected set; }
+
+        public abstract double Area();
+
+        public override string ToString()
+        {
+            return this.Type + " площадью " + this.Area().ToString();
+        }
+    }
+
+    interface IPrint
+    {
+        void Print();
+    }
+
+    class Rectangle : GeometricFigure, IPrint
+    {
+
+        public double Height { get; protected set; }
+        public double Width { get; protected set; }
+
+        public Rectangle(double ph, double pw)
+        {
+            this.Height = ph;
+            this.Width = pw;
+            this.Type = "Прямоугольник";
+        }
+
+        public override double Area()
+        {
+            return (this.Height * this.Width);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+
+    class Square: Rectangle, IPrint
+    {
+        public Square(double size) : base(size, size)
+        {
+            this.Type = "Квадрат";
+        }
+    }
+
+    class Circle : GeometricFigure, IPrint
+    {
+        public double Radius { get; protected set; }
+
+        public Circle(double pr)
+        {
+            this.Radius = pr;
+            this.Type = "Круг";
+        }
+
+        public override double Area()
+        {
+            return this.Radius * this.Radius * Math.PI;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Figures;
+
+namespace lab2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(2, 3);
+            rectangle.Print();
+
+            Square square = new Square(5);
+            square.Print();
+
+            Circle circle = new Circle(4);
+            circle.Print();
+        }
+    }
+}
